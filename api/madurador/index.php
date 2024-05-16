25485f564a62ec2569e1f16ae45c92b5248299f9
ztrackperu <ztrack@zgroup.com.pe>
2024-05-16 12:48:57 -0500

    con oxxo

--- api/madurador/index.php
+++ api/madurador/index.php
@@ -270,7 +270,12 @@ if($primerFiltro =="Madurador"){
     if($nombrecontenedor=='ZGRU1011436' || $nombrecontenedor=='ZGRU1011456'||$nombrecontenedor=='ZGRU1011452'||$nombrecontenedor=='ZGRU1011449'||$nombrecontenedor=='ZGRU1011451'||$nombrecontenedor=='ZGRU8708448'||$nombrecontenedor=='ZGRU8718826'){
         $latitud = "27.739717";
         $longitud="-80.658852";
+    }
+    if($nombrecontenedor=='ZGRU6357290' || $nombrecontenedor=='ZGRU5014794'){
+        $latitud = "-12.210452";
+        $longitud="-76.96628";
         }
+
     //$alarm_number = $contenedor->alarm_number;
     $numero_telefono =  $contenedor->nombre_contenedor;
     $imei =  $contenedor->nombre_contenedor;
